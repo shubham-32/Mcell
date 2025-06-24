6c2eeaad1abbdc94af65e172605d8e9a4a3bd285
Shubham Malik <158245681+shubham-32@users.noreply.github.com>
2025-06-24 14:45:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 
-// Load environment variables
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
-// Middleware
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
-app.use(express.json());
-app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
-// Routes
 app.use("/api/auth", authRoutes);
+
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -30,7 +26,6 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+	console.log("Server is running on port: ", PORT);
 });
