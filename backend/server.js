5266886e2ec09a20cbd9d5af8f000808b7cf74fd
Shubham Malik <158245681+shubham-32@users.noreply.github.com>
2025-06-24 15:12:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://mcell.onrender.com", credentials: true }));
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
